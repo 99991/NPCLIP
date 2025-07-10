28bbf32c06fe147c86b0ff368a9dd34c947e76eb
99991 <99991@users.noreply.github.com>
2025-07-10 17:44:32 +0200

    Rename to npclip

--- example.py
+++ example.py
@@ -1,17 +1,17 @@
-import numpyclip
+import npclip
 import numpy as np
 from PIL import Image
 
-model, preprocess = numpyclip.load("ViT-B/32")
+model, preprocess = npclip.load("ViT-B/32")
 
 image = preprocess(Image.open("data/CLIP.png"))[np.newaxis, :, :, :]
 
-text = numpyclip.tokenize(["a diagram", "a dog", "a cat"])
+text = npclip.tokenize(["a diagram", "a dog", "a cat"])
 
 image_features = model.encode_image(image)
 text_features = model.encode_text(text)
 
 logits_per_image, logits_per_text = model(image, text)
-probs = numpyclip.softmax(logits_per_image, axis=-1)
+probs = npclip.softmax(logits_per_image, axis=-1)
 
 print("Label probs:", probs)  # prints: [[0.99279356 0.00421067 0.00299573]]
--- tests.py
+++ tests.py
@@ -1,20 +1,20 @@
-import numpyclip
+import npclip
 from PIL import Image
 import numpy as np
 import csv
 
-model, preprocess = numpyclip.load("ViT-B/32")
+model, preprocess = npclip.load("ViT-B/32")
 
 def test_features():
     image = preprocess(Image.open("data/CLIP.png"))[np.newaxis, :, :, :]
 
-    text = numpyclip.tokenize(["a diagram", "a dog", "a cat"])
+    text = npclip.tokenize(["a diagram", "a dog", "a cat"])
 
     image_features = model.encode_image(image)
     text_features = model.encode_text(text)
 
     logits_per_image, logits_per_text = model(image, text)
-    probs = numpyclip.softmax(logits_per_image, axis=-1)
+    probs = npclip.softmax(logits_per_image, axis=-1)
 
     expected_text_features = np.load("data/text_features.npy")
     expected_image_features = np.load("data/image_features.npy")
@@ -30,7 +30,7 @@ with open("data/labels.csv", encoding="utf-8") as f:
 def test_classification():
     d = {label: filename for filename, label in data}
     labels = list(d.keys())
-    text = numpyclip.tokenize(["a photo of " + label for label in labels])
+    text = npclip.tokenize(["a photo of " + label for label in labels])
 
     text_features = model.encode_text(text)
     text_features = text_features / np.linalg.norm(text_features, axis=1, keepdims=True)
@@ -47,7 +47,7 @@ def test_classification():
         scale = np.exp(model.params["logit_scale"])
         logits_per_image = scale * image_features @ text_features.T
 
-        probs = numpyclip.softmax(logits_per_image, axis=-1)
+        probs = npclip.softmax(logits_per_image, axis=-1)
 
         index = np.argmax(probs)
 
